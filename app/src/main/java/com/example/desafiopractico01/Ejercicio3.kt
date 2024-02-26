60ee1964a2dd8d78938cda2ce340f001a3563888
Chris Ardon <rockrisbl@gmail.com>
2024-02-25 18:45:40 -0600

    Ejercicio 3 finalizado <3

--- app/src/main/java/com/example/desafiopractico01/Ejercicio3.kt
+++ app/src/main/java/com/example/desafiopractico01/Ejercicio3.kt
@@ -7,5 +7,47 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
-class Ejercicio3 {
+class Ejercicio3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ejercicio3)
+        //DECLARAMOS LAS VARIABLES
+
+        val num1 = findViewById<EditText>(R.id.txtNum1)
+        val num2 = findViewById<EditText>(R.id.txtNum2)
+        val suma = findViewById<Button>(R.id.BtnSuma)
+        val resta = findViewById<Button>(R.id.BtnResta)
+        val multiplicacion = findViewById<Button>(R.id.BtnMultiplicacion)
+        val division = findViewById<Button>(R.id.BtnDivision)
+        val Resultado = findViewById<TextView>(R.id.LblResultado)
+
+        suma.setOnClickListener {
+            var num_1: String = num1.text.toString()
+            val num_2: String = num2.text.toString()
+            val Total = num_1.toFloat() + num_2.toFloat()
+            Resultado.setText("Resultado: "+Total)
+        }
+
+        resta.setOnClickListener {
+            var num_1: String = num1.text.toString()
+            val num_2: String = num2.text.toString()
+            val Total = num_1.toFloat() - num_2.toFloat()
+            Resultado.setText("Resultado: "+Total)
+        }
+
+        multiplicacion.setOnClickListener {
+            var num_1: String = num1.text.toString()
+            val num_2: String = num2.text.toString()
+            val Total = num_1.toFloat() * num_2.toFloat()
+            Resultado.setText("Resultado: "+Total)
+        }
+
+        division.setOnClickListener {
+            var num_1: String = num1.text.toString()
+            val num_2: String = num2.text.toString()
+            val Total = num_1.toFloat() / num_2.toFloat()
+            Resultado.setText("Resultado: "+Total)
+        }
+
+    }
 }
